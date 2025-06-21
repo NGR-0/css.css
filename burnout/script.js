29aba5a89d89e04eb0f5f3cfff6c77152fa59734
yogg <nugrohy55@gmail.com>
2025-06-21 22:17:25 +0700

    add code burnout

--- burnout/script.js
+++ burnout/script.js
@@ -1,23 +1,55 @@
 const smokeContainer = document.querySelector('.smoke');
-const animateSmoke = ['smk1', 'smk2', 'smk3', 'smk4', 'smk5', 'smk6', 'smk7', 'smk8', 'smk9', 'smk10','smk11','smk12','smk13','smk14','smk15'];
+const animateSmoke = [
+    'smk1', 'smk2', 'smk3', 'smk4', 'smk5',
+    'smk6', 'smk7', 'smk8', 'smk9', 'smk10',
+    'smk11', 'smk12', 'smk13', 'smk14', 'smk15'
+];
+
+let smokeInterval;
+let props = 1;
 
 function createSmoke(props) {
     const smoke = document.createElement('span');
-
     const randomIndex = Math.floor(Math.random() * animateSmoke.length);
     const animationName = animateSmoke[randomIndex];
-    const duration = Math.random() * 3+1;
+    const duration = Math.random() * 3 + 1;
+    const delay = props * -1.5;
 
     smoke.style.setProperty('--i', props);
-    const delay = props * -1;
-
     smoke.style.animationDelay = `${delay}s`;
-    smoke.style.animation = `${animationName} ${duration}s linear infinite`;
+    smoke.style.animation = `${animationName} ${duration}s linear forwards infinite`;
 
     smokeContainer.appendChild(smoke);
 
+    if (smokeContainer.children.length > 61) {
+        smokeContainer.removeChild(smokeContainer.firstChild);
+    }
 }
 
-for (let i = 1; i < 61; i++) {
-    createSmoke(i);
+function startSmoke(rate) {
+    clearInterval(smokeInterval);
+    smokeInterval = setInterval(() => {
+        createSmoke(props++);
+    }, rate);
 }
+function stopSmoke(rate) {
+    clearInterval(smokeInterval);
+    smokeInterval = setInterval(() => {
+        smokeContainer.removeChild(smokeContainer.firstChild);
+    }, rate);
+}
+
+function SmokeCycle() {
+
+    setTimeout(() => {
+        startSmoke(100);
+    }, 2000);
+
+    setTimeout(() => {
+        stopSmoke(50);
+    }, 15000);
+
+    setTimeout(SmokeCycle, 20000);
+}
+
+SmokeCycle()
